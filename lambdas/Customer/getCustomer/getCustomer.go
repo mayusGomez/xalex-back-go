381f7713f659f4cd9c427dc98299596a24b07d60
Alexander Gomez Higuita <agomez@payvalida.com>
2021-04-14 00:00:31 -0500

    Get Customer lambda

---
+++ lambdas/Customer/getCustomer/getCustomer.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/mayusGomez/xalex/customers"
+	"github.com/mayusGomez/xalex/customers/services"
+	"github.com/mayusGomez/xalex/customers/storage/mongo"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(LambdaHandler)
+}
+
+func LambdaHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	customerId := request.PathParameters["customerId"]
+	log.Printf("request.customerId: %+v", customerId)
+
+	if customerId == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       createResponse(&customers.Customer{}),
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	storage := mongo.MongoStorage{
+		StringConn: os.Getenv("STR_MONGO_CONN"),
+		Context:    ctx,
+	}
+	err := storage.Connect()
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       createResponse(&customers.Customer{}),
+			StatusCode: http.StatusInternalServerError,
+		}, nil
+	}
+	defer storage.Disconnect()
+
+	customer, err := services.GetCustomer(customerId, &storage)
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       createResponse(&customers.Customer{}),
+			StatusCode: http.StatusInternalServerError,
+		}, nil
+	}
+
+	log.Println("Result customer:", customer)
+
+	if customer.ID == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       createResponse(&customer),
+			StatusCode: http.StatusNotFound,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       createResponse(&customer),
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func createResponse(customer *customers.Customer) string {
+
+	data, _ := json.Marshal(customer)
+	return string(data)
+
+}
